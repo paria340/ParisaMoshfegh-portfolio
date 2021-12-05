744172f16a774dee7235ee5d5b67222681a905e6
paria340 <paria340@gmail.com>
2021-12-04 16:14:37 -0800

    a

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
 
+functionality = {}
 
-// $('#simple_arc').circleType({radius:135});
+functionality.onSubmit = function(){
+    console.log('hello')
+    const formEl = document.querySelector('form')
+    formEl.addEventListener('submit', function(event){
+        event.preventDefault();
+        console.log('hello from here')
+    })
+}
+
+functionality.init = function(){
+    functionality.onSubmit()
+}
+
+functionality.init()
+
+
