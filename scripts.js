1e95e7d2f7f60f25f356b8e2d68791e462d85e27
paria340 <paria340@gmail.com>
2021-12-04 23:58:38 -0800

    s

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
 
 functionality = {}
 
-functionality.onSubmit = function(){
-    
+functionality.onSubmit = function () {
+
     const formEl = document.querySelector('form')
-    formEl.addEventListener('submit', function(event){
+    formEl.addEventListener('submit', function (event) {
         event.preventDefault();
         let name = document.querySelector('.fullName').value
         let email = document.querySelector('.email').value
         let message = document.querySelector('.message').value
-
         formEl.reset()
-
         sendEmail(name, email, message)
     })
 }
@@ -28,8 +26,18 @@ function sendEmail(name, email, message) {
     }).then((message) => alert('the mail has been sent'))
 }
 
-functionality.init = function(){
+functionality.scroll = function () {
+    const button = document.querySelector('.arrow')
+    button.addEventListener('click', function () {
+        window.scroll({top: 1000, left: 0, behavior: 'smooth'});
+    });
+}
+
+
+
+functionality.init = function () {
     functionality.onSubmit()
+    functionality.scroll()
 }
 
 functionality.init()
