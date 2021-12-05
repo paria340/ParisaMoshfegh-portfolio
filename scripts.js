c9bcc01ba6a5c4b0d151e1fef82c4a84201961d7
paria340 <paria340@gmail.com>
2021-12-04 17:20:17 -0800

    m

--- scripts.js
+++ scripts.js
@@ -6,8 +6,26 @@ functionality.onSubmit = function(){
     const formEl = document.querySelector('form')
     formEl.addEventListener('submit', function(event){
         event.preventDefault();
-        console.log('hello from here')
+        let name = document.querySelector('.fullName').value
+        let email = document.querySelector('.email').value
+        let message = document.querySelector('.message')
+
+        // formEl.reset()
+
+        sendEmail(name, email, message)
     })
+}
+
+function sendEmail(name, email, message) {
+    Email.send({
+        Host: 'smtp.gmail.com',
+        Username: 'paria340@gmail.com',
+        Password: 'yfdydjldyyqxszhk',
+        To: 'paria340@gmail.com',
+        From: 'paria340@gmail.com',
+        Subject: `${name} sent you a message`,
+        Body: `Name ${name} Email: ${email} Message: ${message}`
+    }).then((message) => alert('the mail has been sent'))
 }
 
 functionality.init = function(){
